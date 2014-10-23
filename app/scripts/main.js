171a5be5dd9f8779a71840ec96d00c0e081290c6
Jess Scheuring <jscheuring@gmail.com>
2014-10-22 22:01:32 -0400

    fixes name conflict w/router

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,7 +8,7 @@
 
   $(document).ready(function() {
     Parse.initialize("n2FkI50T90q2AKTnXiwuWo1Rl3KeCkMRek7OHQoR", "ZDSSQlJysTp3GKz3fzKE0O1WZKAEEuuyoPhv8CHc");
-    window.Anypic = new Anypic.Router();
+    window.AnypicRouter = new Anypic.Router();
     Parse.history.start();
   });
 
